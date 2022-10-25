2cd43b33d9d01a6fb5affb0aec39aed7fda1cea2
rob <rob.van.bommel@me.com>
2022-10-25 20:07:58 +0200

    added XYZ check

--- XYZ_wing.py
+++ XYZ_wing.py
@@ -33,6 +33,7 @@ If so, the common entry in Z1 and Z2 is the Z_value
 
 """
 
+
 import pandas as pd
 
 column_names = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I']
@@ -41,7 +42,7 @@ df = pd.read_excel(io='data/SDK.xlsx', sheet_name='python', header=None, index_c
 df = df.iloc[:9, :9]
 df.index += 1
 df = df.fillna(999999)  # to avoid nan,which makes string type impossible: must be 6'9', so exclude them from quintuplet
-df_str = df.astype(str)  # str is used later to be able to separate 126 in a cell into '1', '2' and '6'
+df_str = df.astype(str)   # str is used later to be able to separate 126 in a cell into '1', '2' and '6'
 
 # box 1
 box_series_1 = pd.Series([''], index=[0])
@@ -143,7 +144,7 @@ for row_box in range(6, 9):
 box_series_9 = box_series_9.drop(labels=[0])
 
 # here loop starts for every cell to do the analysis on
-for row in range(9):  # for each cell create buddy_list. First add this cell as first entry in BL, then row, col, box
+for row in range(9):   # for each cell create buddy_list. First add this cell as first entry in BL, then row, col, box
     for column in range(9):
         cell_start_row_column_index = str(row + 1) + str(column + 1)  # row_column cell is pot. XY ==> top buddy list
         cell_start_row_column = df_str.iloc[row, column]
@@ -151,13 +152,13 @@ for row in range(9):  # for each cell create buddy_list. First add this cell as 
         buddy_series = pd.Series([cell_start_row_column_sep], index=[int(cell_start_row_column_index)])
         for i in range(9):
             cell_index = str(row + 1) + str(i + 1)
-            cell = df_str.iloc[row, i]  # loop through each cell in column
-            cell_sep = list(cell)  # each double number as 2 separate numbers in string (26 = '2','6')
+            cell = df_str.iloc[row, i]              # loop through each cell in column
+            cell_sep = list(cell)                   # each double number as 2 separate numbers in string (26 = '2','6')
             series_temp = pd.Series([cell_sep], index=[int(cell_index)])
             buddy_series = buddy_series.append(series_temp)
         for j in range(9):
             cell_index = str(j + 1) + str(column + 1)
-            cell = df_str.iloc[j, column]  # loop through each cell in row
+            cell = df_str.iloc[j, column]           # loop through each cell in row
             cell_sep = list(cell)
             series_temp = pd.Series([cell_sep], index=[int(cell_index)])
             buddy_series = buddy_series.append(series_temp)
@@ -182,8 +183,9 @@ for row in range(9):  # for each cell create buddy_list. First add this cell as 
         if row // 3 == 2 and column // 3 == 2:
             correct_box = box_series_9
 
-        buddy_series = buddy_series.append(correct_box)  # add correct box to buddy_list
-        index = buddy_series.index  # as you add row,col,box ==> double entries: remove double entries
+
+        buddy_series = buddy_series.append(correct_box)   # add correct box to buddy_list
+        index = buddy_series.index                    # as you add row,col,box ==> double entries: remove double entries
         buddy_series_unique = buddy_series[~index.duplicated(keep='first')]
 
         # XY WING
@@ -198,14 +200,14 @@ for row in range(9):  # for each cell create buddy_list. First add this cell as 
             z_both_numbers_list = []
             # for all potential Z_cells in list (not first entry, because that is XY_cell, so range (1 to 21)
             for potential_Z in range(1, 21):
-                if len(buddy_series_unique.iloc[potential_Z]) == 2:  # check _cells has 2 entries
+                if len(buddy_series_unique.iloc[potential_Z]) == 2:   # check _cells has 2 entries
                     if buddy_series_unique.iloc[potential_Z] != buddy_series_unique.iloc[0]:  # exclude if pair with XY
                         if (x in buddy_series_unique.iloc[potential_Z]) or (y in buddy_series_unique.iloc[potential_Z]):
                             # make copy of buddy_series_unique, so buddy_s_u not changed if you remove X and Y
                             z_temp = buddy_series_unique.iloc[potential_Z].copy()
-                            z_index_temp = [buddy_series_unique.index[potential_Z]]  # get cell index of z
+                            z_index_temp = [buddy_series_unique.index[potential_Z]]   # get cell index of z
                             z_concat = str(int(''.join(z_temp)))
-                            z_both_numbers_list = z_both_numbers_list + [z_concat]  # concat to make '7' '8' ==> 78
+                            z_both_numbers_list = z_both_numbers_list + [z_concat]    # concat to make '7' '8' ==> 78
                             if x in z_temp:
                                 z_temp.remove(x)
                             if y in z_temp:
@@ -217,13 +219,12 @@ for row in range(9):  # for each cell create buddy_list. First add this cell as 
             # first check if 2 Z_cells are NOT a pair and have same Z_value
             # then check that Z_1 and Z_2 are NOT buddies of each other (because then not a XY Wing!!)
 
-            number_candidates = len(z_both_numbers_list)  # make all possible combinations of 2 Z_cells
+            number_candidates = len(z_both_numbers_list)                  # make all possible combinations of 2 Z_cells
             if len(z_both_numbers_list) > 1:
                 for check_number_1 in range(number_candidates - 1):
                     for check_number_2 in range(1, number_candidates):
                         if check_number_1 + check_number_2 <= len(z_both_numbers_list) - 1:
-                            if z_both_numbers_list[check_number_1] != z_both_numbers_list[
-                                check_number_1 + check_number_2]:
+                            if z_both_numbers_list[check_number_1] != z_both_numbers_list[check_number_1 + check_number_2]:
                                 if z_list[check_number_1] == z_list[check_number_1 + check_number_2]:  # check same z_
                                     # On XY index get the Row and Column (needed for buddy check each Z_cell & XY_cell)
                                     xy_index_sep = list(str(xy_index))
@@ -242,8 +243,7 @@ for row in range(9):  # for each cell create buddy_list. First add this cell as 
 
                                             if z_index_list_first_entry_sep[1] != z_index_list_second_entry_sep[1]:
                                                 print('\nXY wing discovered')
-                                                print('xy index is ' + str(xy_index) + ', cell content ' + str(x) + str(
-                                                    y))
+                                                print('xy index is ' + str(xy_index) + ', cell content ' + str(x) + str(y))
 
                                                 print('Z1 index is ' + str(int(''.join(z_index_list_first_entry_sep)))
                                                       + ', cell content ' + str(
@@ -254,7 +254,8 @@ for row in range(9):  # for each cell create buddy_list. First add this cell as 
                                                 print('Eliminate Z_Value ' + str(z_list[check_number_1]) +
                                                       ' in all buddy cells to both Z1 and Z2')
 
-        #  START ON XYZ WING
+
+#  START ON XYZ WING
         # all variables now have CAPITALS
         # if first cell indeed has 3 entries, it can be X, Y, Z: you don't know which one is Z till check on Z1 and Z2
         if len(buddy_series_unique.iloc[0]) == 3:
@@ -262,27 +263,24 @@ for row in range(9):  # for each cell create buddy_list. First add this cell as 
             second_number = buddy_series_unique.iloc[0][1]
             third_number = buddy_series_unique.iloc[0][2]
             XYZ_index = buddy_series_unique.index[0]
-            Z_list = []  # Z_cell, don't know yet which (Z) in other Z_cell, so both nrs added in separate
+            Z_list = []   # Z_cell, don't know yet which (Z) in other Z_cell, so both nrs added in separate
             Z_index_list = []
 
             # for all potential Z_cells in list (not first entry, because that is XYZ_cell, so range (1 to 21)
             for potential_Z in range(1, 21):
-                if len(buddy_series_unique.iloc[potential_Z]) == 2:  # check _cells has 2 entries
-                    if (first_number in buddy_series_unique.iloc[potential_Z]) and (
-                            second_number in buddy_series_unique.iloc[potential_Z]) \
-                            or (first_number in buddy_series_unique.iloc[potential_Z]) and (
-                            third_number in buddy_series_unique.iloc[potential_Z]) \
-                            or (second_number in buddy_series_unique.iloc[potential_Z]) and (
-                            third_number in buddy_series_unique.iloc[potential_Z]):
+                if len(buddy_series_unique.iloc[potential_Z]) == 2:   # check _cells has 2 entries
+                    if (first_number in buddy_series_unique.iloc[potential_Z]) and (second_number in buddy_series_unique.iloc[potential_Z])\
+                            or (first_number in buddy_series_unique.iloc[potential_Z]) and (third_number in buddy_series_unique.iloc[potential_Z])\
+                            or (second_number in buddy_series_unique.iloc[potential_Z]) and (third_number in buddy_series_unique.iloc[potential_Z]):
                         # make copy of buddy_series_unique, so buddy_s_u not changed if you remove X and Y
                         Z_temp = buddy_series_unique.iloc[potential_Z].copy()
                         Z_list = Z_list + [Z_temp]  # still separated '7' '8' to do check
-                        Z_index_temp = [buddy_series_unique.index[potential_Z]]  # get cell index of z
+                        Z_index_temp = [buddy_series_unique.index[potential_Z]]   # get cell index of z
                         Z_index_list = Z_index_list + Z_index_temp
 
             # MAKE ALL POSSIBLE COMBINATIONS OF Z POTENTIALS
             number_candidates_XYZ = len(Z_list)
-            if len(Z_list) > 1:  # make sure at least 2 Z_candidates
+            if len(Z_list) > 1:   # make sure at least 2 Z_candidates
                 for check_number_1 in range(number_candidates_XYZ - 1):
                     for check_number_2 in range(1, number_candidates_XYZ):
                         if check_number_1 + check_number_2 <= number_candidates_XYZ - 1:
@@ -292,15 +290,13 @@ for row in range(9):  # for each cell create buddy_list. First add this cell as 
                                 print(Z_list[check_number_1 + check_number_2])
                                 if (Z_list[0][0]) in Z_list[1]:
                                     print("\nZYZ wing discovered")
-                                    print('XYZ index is ' + str(XYZ_index) + ', cell content ' + str(
-                                        list(first_number + second_number + third_number)))
+                                    print('XYZ index is ' + str(XYZ_index) + ', cell content ' + str(list(first_number + second_number + third_number)))
                                     print('Z_1 index is ' + str(Z_index_list[0]) + ', cell content ' + str(Z_list[0]))
                                     print('Z_2 index is ' + str(Z_index_list[1]) + ', cell content ' + str(Z_list[1]))
                                     print('Eliminate ' + str(Z_list[0][0]) + ' in all buddy cells to both Z_1 and Z_2')
                                 else:
                                     print("\nXYZ wing discovered")
-                                    print('XYZ index is ' + str(XYZ_index) + ', cell content ' + str(
-                                        list(first_number + second_number + third_number)))
+                                    print('XYZ index is ' + str(XYZ_index) + ', cell content ' + str(list(first_number + second_number + third_number)))
                                     print('Z_1 index is ' + str(Z_index_list[0]) + ' cell content ' + str(Z_list[0]))
                                     print('Z_2 index is ' + str(Z_index_list[1]) + ' cell content ' + str(Z_list[1]))
                                     print('Eliminate ' + str(Z_list[0][1]) + ' in all buddy cells to both Z_1 and Z_2')
